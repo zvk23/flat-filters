a876c6597ccc6a029b719786ec653862a9bb84b1
Efimenko VE <efimenko.ve@nsd.ru>
2025-10-02 19:27:03 +0300

    Добавлен модуль @nuxt/fonts и подключил кастомный шрифт

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     Init apertment filters app
   </div>
 </template>
+
+<style lang="scss">
+@use "~/assets/scss/main.scss";
+
+html {
+
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
+import { devtools } from "vue";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  // css: ['~/assets/css/main.css'],
+  fonts: {
+    devtools: true,
+    families: [
+      { name: 'PT ROOT', provider: 'none', src: ['~/assets/fonts/pt-root-ui_regular.woff', '~/assets/fonts/pt-root-ui_regular.woff2'], weight: 'normal', global: true },
+      { name: 'PT ROOT', provider: 'none', src: ['~/assets/fonts/pt-root-ui_bold.woff', '~/assets/fonts/pt-root-ui_bold.woff2'], weight: 'bold' },
+    ]
+  }
 })
